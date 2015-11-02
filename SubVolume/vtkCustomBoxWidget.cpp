32776ad9045a4c33736d0a42e19fb76b7085020d
Sabreme <the.token.blackman@gmail.com>
2015-11-02 14:33:14 +0200

    DONE: Shortened vtkCustomBoxWidget to include Right and Middle Mouse Reverse DONE: Mouse Pre-Pilot Bugs COMPLETE
    TODO: Leap Pre-Pilot Bugs

---
+++ SubVolume/vtkCustomBoxWidget.cpp
@@ -0,0 +1,413 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkCustomBoxWidget.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkCustomBoxWidget.h"
+
+#include "vtkActor.h"
+#include "vtkAssemblyNode.h"
+#include "vtkAssemblyPath.h"
+#include "vtkCallbackCommand.h"
+#include "vtkCamera.h"
+#include "vtkCellArray.h"
+#include "vtkCellPicker.h"
+#include "vtkDoubleArray.h"
+#include "vtkFloatArray.h"
+#include "vtkMath.h"
+#include "vtkObjectFactory.h"
+#include "vtkPlanes.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderer.h"
+#include "vtkSphereSource.h"
+#include "vtkTransform.h"
+
+vtkStandardNewMacro(vtkCustomBoxWidget)
+
+vtkCustomBoxWidget::vtkCustomBoxWidget()
+{
+  this->State = vtkCustomBoxWidget::Start;
+  this->EventCallbackCommand->SetCallback(vtkCustomBoxWidget::ProcessEvents);
+
+  // Enable/disable the translation, rotation, and scaling of the widget
+  this->TranslationEnabled = 1;
+  this->RotationEnabled = 1;
+  this->ScalingEnabled = 1;
+
+  //Build the representation of the widget
+  int i;
+
+  // Control orientation of normals
+  this->InsideOut = 0;
+  this->OutlineFaceWires = 0;
+  this->OutlineCursorWires = 1;
+
+  // Construct the poly data representing the hex
+  this->HexPolyData = vtkPolyData::New();
+  this->HexMapper = vtkPolyDataMapper::New();
+  this->HexMapper->SetInput(HexPolyData);
+  this->HexActor = vtkActor::New();
+  this->HexActor->SetMapper(this->HexMapper);
+
+  // Construct initial points
+  this->Points = vtkPoints::New(VTK_DOUBLE);
+  this->Points->SetNumberOfPoints(15);//8 corners; 6 faces; 1 center
+  this->HexPolyData->SetPoints(this->Points);
+
+  // Construct connectivity for the faces. These are used to perform
+  // the picking.
+  vtkIdType pts[4];
+  vtkCellArray *cells = vtkCellArray::New();
+  cells->Allocate(cells->EstimateSize(6,4));
+  pts[0] = 3; pts[1] = 0; pts[2] = 4; pts[3] = 7;
+  cells->InsertNextCell(4,pts);
+  pts[0] = 1; pts[1] = 2; pts[2] = 6; pts[3] = 5;
+  cells->InsertNextCell(4,pts);
+  pts[0] = 0; pts[1] = 1; pts[2] = 5; pts[3] = 4;
+  cells->InsertNextCell(4,pts);
+  pts[0] = 2; pts[1] = 3; pts[2] = 7; pts[3] = 6;
+  cells->InsertNextCell(4,pts);
+  pts[0] = 0; pts[1] = 3; pts[2] = 2; pts[3] = 1;
+  cells->InsertNextCell(4,pts);
+  pts[0] = 4; pts[1] = 5; pts[2] = 6; pts[3] = 7;
+  cells->InsertNextCell(4,pts);
+  this->HexPolyData->SetPolys(cells);
+  cells->Delete();
+  this->HexPolyData->BuildCells();
+
+  // The face of the hexahedra
+  cells = vtkCellArray::New();
+  cells->Allocate(cells->EstimateSize(1,4));
+  cells->InsertNextCell(4,pts); //temporary, replaced later
+  this->HexFacePolyData = vtkPolyData::New();
+  this->HexFacePolyData->SetPoints(this->Points);
+  this->HexFacePolyData->SetPolys(cells);
+  this->HexFaceMapper = vtkPolyDataMapper::New();
+  this->HexFaceMapper->SetInput(HexFacePolyData);
+  this->HexFace = vtkActor::New();
+  this->HexFace->SetMapper(this->HexFaceMapper);
+  cells->Delete();
+
+  // Create the outline for the hex
+  this->OutlinePolyData = vtkPolyData::New();
+  this->OutlinePolyData->SetPoints(this->Points);
+  this->OutlineMapper = vtkPolyDataMapper::New();
+  this->OutlineMapper->SetInput(this->OutlinePolyData);
+  this->HexOutline = vtkActor::New();
+  this->HexOutline->SetMapper(this->OutlineMapper);
+  cells = vtkCellArray::New();
+  cells->Allocate(cells->EstimateSize(15,2));
+  this->OutlinePolyData->SetLines(cells);
+  cells->Delete();
+
+  // Set up the initial properties
+  this->CreateDefaultProperties();
+
+  // Create the outline
+  this->GenerateOutline();
+
+  // Create the handles
+  this->Handle = new vtkActor* [7];
+  this->HandleMapper = new vtkPolyDataMapper* [7];
+  this->HandleGeometry = new vtkSphereSource* [7];
+  for (i=0; i<7; i++)
+    {
+    this->HandleGeometry[i] = vtkSphereSource::New();
+    this->HandleGeometry[i]->SetThetaResolution(16);
+    this->HandleGeometry[i]->SetPhiResolution(8);
+    this->HandleMapper[i] = vtkPolyDataMapper::New();
+    this->HandleMapper[i]->SetInput(this->HandleGeometry[i]->GetOutput());
+    this->Handle[i] = vtkActor::New();
+    this->Handle[i]->SetMapper(this->HandleMapper[i]);
+    }
+
+  // Define the point coordinates
+  double bounds[6];
+  bounds[0] = -0.5;
+  bounds[1] = 0.5;
+  bounds[2] = -0.5;
+  bounds[3] = 0.5;
+  bounds[4] = -0.5;
+  bounds[5] = 0.5;
+  // Points 8-14 are down by PositionHandles();
+  this->PlaceWidget(bounds);
+
+  //Manage the picking stuff
+  this->HandlePicker = vtkCellPicker::New();
+  this->HandlePicker->SetTolerance(0.001);
+  for (i=0; i<7; i++)
+    {
+    this->HandlePicker->AddPickList(this->Handle[i]);
+    }
+  this->HandlePicker->PickFromListOn();
+
+  this->HexPicker = vtkCellPicker::New();
+  this->HexPicker->SetTolerance(0.001);
+  this->HexPicker->AddPickList(HexActor);
+  this->HexPicker->PickFromListOn();
+
+  this->CurrentHandle = NULL;
+
+  this->Transform = vtkTransform::New();
+}
+
+vtkCustomBoxWidget::~vtkCustomBoxWidget()
+{
+  this->HexActor->Delete();
+  this->HexMapper->Delete();
+  this->HexPolyData->Delete();
+  this->Points->Delete();
+
+  this->HexFace->Delete();
+  this->HexFaceMapper->Delete();
+  this->HexFacePolyData->Delete();
+
+  this->HexOutline->Delete();
+  this->OutlineMapper->Delete();
+  this->OutlinePolyData->Delete();
+  
+  for (int i=0; i<7; i++)
+    {
+    this->HandleGeometry[i]->Delete();
+    this->HandleMapper[i]->Delete();
+    this->Handle[i]->Delete();
+    }
+  delete [] this->Handle;
+  delete [] this->HandleMapper;
+  delete [] this->HandleGeometry;
+  
+  this->HandlePicker->Delete();
+  this->HexPicker->Delete();
+
+  this->Transform->Delete();
+  
+  this->HandleProperty->Delete();
+  this->SelectedHandleProperty->Delete();
+  this->FaceProperty->Delete();
+  this->SelectedFaceProperty->Delete();
+  this->OutlineProperty->Delete();
+  this->SelectedOutlineProperty->Delete();
+}
+
+void vtkCustomBoxWidget::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+
+  double *bounds=this->InitialBounds;
+  os << indent << "Initial Bounds: "
+     << "(" << bounds[0] << "," << bounds[1] << ") "
+     << "(" << bounds[2] << "," << bounds[3] << ") " 
+     << "(" << bounds[4] << "," << bounds[5] << ")\n";
+
+  if ( this->HandleProperty )
+    {
+    os << indent << "Handle Property: " << this->HandleProperty << "\n";
+    }
+  else
+    {
+    os << indent << "Handle Property: (none)\n";
+    }
+  if ( this->SelectedHandleProperty )
+    {
+    os << indent << "Selected Handle Property: " 
+       << this->SelectedHandleProperty << "\n";
+    }
+  else
+    {
+    os << indent << "SelectedHandle Property: (none)\n";
+    }
+
+  if ( this->FaceProperty )
+    {
+    os << indent << "Face Property: " << this->FaceProperty << "\n";
+    }
+  else
+    {
+    os << indent << "Face Property: (none)\n";
+    }
+  if ( this->SelectedFaceProperty )
+    {
+    os << indent << "Selected Face Property: " 
+       << this->SelectedFaceProperty << "\n";
+    }
+  else
+    {
+    os << indent << "Selected Face Property: (none)\n";
+    }
+
+  if ( this->OutlineProperty )
+    {
+    os << indent << "Outline Property: " << this->OutlineProperty << "\n";
+    }
+  else
+    {
+    os << indent << "Outline Property: (none)\n";
+    }
+  if ( this->SelectedOutlineProperty )
+    {
+    os << indent << "Selected Outline Property: " 
+       << this->SelectedOutlineProperty << "\n";
+    }
+  else
+    {
+    os << indent << "Selected Outline Property: (none)\n";
+    }
+
+  os << indent << "Outline Face Wires: " << (this->OutlineFaceWires ? "On\n" : "Off\n");
+  os << indent << "Outline Cursor Wires: " << (this->OutlineCursorWires ? "On\n" : "Off\n");
+  os << indent << "Inside Out: " << (this->InsideOut ? "On\n" : "Off\n");
+  os << indent << "Translation Enabled: " << (this->TranslationEnabled ? "On\n" : "Off\n");
+  os << indent << "Scaling Enabled: " << (this->ScalingEnabled ? "On\n" : "Off\n");
+  os << indent << "Rotation Enabled: " << (this->RotationEnabled ? "On\n" : "Off\n");
+  
+}
+
+#define VTK_AVERAGE(a,b,c) \
+  c[0] = (a[0] + b[0])/2.0; \
+  c[1] = (a[1] + b[1])/2.0; \
+  c[2] = (a[2] + b[2])/2.0;
+
+void vtkCustomBoxWidget::OnRightButtonDown()
+{
+  int X = this->Interactor->GetEventPosition()[0];
+  int Y = this->Interactor->GetEventPosition()[1];
+
+  // Okay, we can process this. Try to pick handles first;
+  // if no handles picked, then pick the bounding box.
+  if (!this->CurrentRenderer || !this->CurrentRenderer->IsInViewport(X, Y))
+    {
+    this->State = vtkCustomBoxWidget::Outside;
+    return;
+    }
+  
+  vtkAssemblyPath *path;
+  this->HandlePicker->Pick(X,Y,0.0,this->CurrentRenderer);
+  path = this->HandlePicker->GetPath();
+  if ( path != NULL )
+    {
+    this->State = vtkCustomBoxWidget::Moving;
+    this->CurrentHandle = this->Handle[6];
+    this->HighlightOutline(1);
+    this->HandlePicker->GetPickPosition(this->LastPickPosition);
+    this->ValidPick = 1;
+    }
+  else
+    {
+    this->HexPicker->Pick(X,Y,0.0,this->CurrentRenderer);
+    path = this->HexPicker->GetPath();
+    if ( path != NULL )
+      {
+      this->State = vtkCustomBoxWidget::Moving;
+      this->CurrentHandle = this->Handle[6];
+      this->HighlightOutline(1);
+      this->HexPicker->GetPickPosition(this->LastPickPosition);
+      this->ValidPick = 1;
+      }
+    else
+      {
+      this->HighlightFace(this->HighlightHandle(NULL));
+      this->State = vtkCustomBoxWidget::Outside;
+      return;
+      }
+    }
+  
+  this->EventCallbackCommand->SetAbortFlag(1);
+  this->StartInteraction();
+  this->InvokeEvent(vtkCommand::StartInteractionEvent, NULL);
+  this->Interactor->Render();
+}
+
+void vtkCustomBoxWidget::OnRightButtonUp()
+{
+  if ( this->State == vtkCustomBoxWidget::Outside ||
+       this->State == vtkCustomBoxWidget::Start )
+    {
+    return;
+    }
+
+  this->State = vtkCustomBoxWidget::Start;
+  this->HighlightFace(this->HighlightHandle(NULL));
+  this->SizeHandles();
+
+  this->EventCallbackCommand->SetAbortFlag(1);
+  this->EndInteraction();
+  this->InvokeEvent(vtkCommand::EndInteractionEvent, NULL);
+  this->Interactor->Render();
+  
+}
+
+void vtkCustomBoxWidget::OnMiddleButtonDown()
+{
+  int X = this->Interactor->GetEventPosition()[0];
+  int Y = this->Interactor->GetEventPosition()[1];
+
+  // Okay, we can process this. Try to pick handles first;
+  // if no handles picked, then pick the bounding box.
+  if (!this->CurrentRenderer || !this->CurrentRenderer->IsInViewport(X, Y))
+    {
+    this->State = vtkCustomBoxWidget::Outside;
+    return;
+    }
+  
+  vtkAssemblyPath *path;
+  this->HandlePicker->Pick(X,Y,0.0,this->CurrentRenderer);
+  path = this->HandlePicker->GetPath();
+  if ( path != NULL )
+    {
+    this->State = vtkCustomBoxWidget::Scaling;
+    this->HighlightOutline(1);
+    this->HandlePicker->GetPickPosition(this->LastPickPosition);
+    this->ValidPick = 1;
+    }
+  else
+    {
+    this->HexPicker->Pick(X,Y,0.0,this->CurrentRenderer);
+    path = this->HexPicker->GetPath();
+    if ( path != NULL )
+      {
+      this->State = vtkCustomBoxWidget::Scaling;
+      this->HighlightOutline(1);
+      this->HexPicker->GetPickPosition(this->LastPickPosition);
+      this->ValidPick = 1;
+      }
+    else
+      {
+      this->State = vtkCustomBoxWidget::Outside;
+      return;
+      }
+    }
+  
+  this->EventCallbackCommand->SetAbortFlag(1);
+  this->StartInteraction();
+  this->InvokeEvent(vtkCommand::StartInteractionEvent, NULL);
+  this->Interactor->Render();
+}
+
+void vtkCustomBoxWidget::OnMiddleButtonUp()
+{
+  if ( this->State == vtkCustomBoxWidget::Outside )
+    {
+    return;
+    }
+
+  this->State = vtkCustomBoxWidget::Start;
+  this->HighlightOutline(0);
+  this->SizeHandles();
+  
+  this->EventCallbackCommand->SetAbortFlag(1);
+  this->EndInteraction();
+  this->InvokeEvent(vtkCommand::EndInteractionEvent, NULL);
+  this->Interactor->Render();
+}
